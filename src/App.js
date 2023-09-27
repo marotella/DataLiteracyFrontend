fa2a4e860082082f12c706dd09c39e03b0526eb9
marotella <michelle.a.rotella@gmail.com>
2023-09-26 21:46:28 -0400

    home page setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, {useEffect, useState} from 'react'
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import Login from ./pages/Login
+import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
   const baseURL = process.env.REACT_APP_BASE_URL
   console.log(baseURL)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home baseURL={baseURL}/>}/>
+        <Route path="/login" element={<Login baseURL={baseURL}/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home () {
+    return(
+        <div>
+            <h1>Data Literacy</h1>
+        </div>
+    )
+}
+
+export default Home
